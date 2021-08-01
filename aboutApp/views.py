423c75f55fa2aa0dd722caa52280b47cbf900ff1
kenlig <28685375+kenlig@users.noreply.github.com>
2021-08-01 10:15:07 +0800

    qwq

--- aboutApp/views.py
+++ aboutApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def servey(request):
+    html='<html><body>企业概况</body></html>'
+    return HttpResponse(html)
+def honor(request):
+    html='<html><body>荣誉资质</body></html>'
+    return HttpResponse(html)
--- homeApp/views.py
+++ homeApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def home(request):
+    html='<html><body>首页</body></html>'
+    return HttpResponse(html)
