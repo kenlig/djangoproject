840a4c46207a6659564e3a101a5f8d4c7ae579bf
kenlig <28685375+kenlig@users.noreply.github.com>
2021-08-02 16:27:55 +0800

    init

--- productsApp/urls.py
+++ productsApp/urls.py
@@ -4,7 +4,5 @@ from django.urls import path
 app_name='productsApp'
 
 urlpatterns=[
-    path('robot/',views.robot,name='robot'),
-    path('monitoring/',views.monitoring,name='monitoring'),
-    path('face/',views.face,name='face'),
+    path('products/<str:productName>/',views.products,name='products'),
 ]
--- productsApp/views.py
+++ productsApp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
-def robot(request):
-    html='<html><body><h1>test</h1></body></html>'
-    return HttpResponse(html)
-def monitoring(request):
-    html='<html><body><h1>test</h1></body></html>'
-    return HttpResponse(html)
-def face(request):
-    html='<html><body><h1>test</h1></body></html>'
-    return HttpResponse(html)
+def products(request,productName):
+    submenu=productName
+    if productName == 'robot':
+        productName='家用机器人'
+    elif productName == 'monitor':
+        productName='智能监控'
+    else:
+        productName='人脸识别解决方案'
+
+    return render(request,'productList.html',{'active_menu':'products','sub_menu':submenu,'productName':productName})
